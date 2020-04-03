9c24818f630c39248137b0663c27352b6c089309
Sascha Lamprecht <sascha_77_77@gmx.de>
2020-04-03 13:07:58 +0200

    Tests

--- Wired-Bot/AppDelegate.swift
+++ Wired-Bot/AppDelegate.swift
@@ -11,18 +11,35 @@ import WiredSwift
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate, ConnectionDelegate {
-    
+    var connection:Connection?
     
     func connectionDidReceiveMessage(connection: Connection, message: P7Message) {
-        if message.name == "wired.chat.say" {
-            if let saidText = message.string(forField: "wired.chat.say") {
-                print(saidText)
-            }
-         }
+        // if the calling connection is the one we opened
+        if connection == self.connection {
+            // if it is a chat message
+            if message.name == "wired.chat.say" {
+                // if the message contains a string in 'wired.chat.say' field
+                if let saidText = message.string(forField: "wired.chat.say") {
+                    // the message message starts with 'Hello'
+                    if saidText.starts(with: "Hello") {
+                        // we auto reply 'Hey'
+                        let response = P7Message(withName: "wired.chat.send_say", spec: connection.spec)
+                        response.addParameter(field: "wired.chat.id", value: UInt32(1))
+                        response.addParameter(field: "wired.chat.say", value: "Hey :-)")
+                        _ = connection.send(message: response)
+                    }
+                }
+             }
+        }
     }
     
     func connectionDidReceiveError(connection: Connection, message: P7Message) {
-        print("Error received")
+        print("Error received: \(message)")
+    }
+    
+    func connectionDisconnected(connection: Connection, error: Error?) {
+        // if we received a disconnect signal, we clear the local connection reference
+        self.connection = nil
     }
 
     
@@ -39,22 +56,38 @@ class AppDelegate: NSObject, NSApplicationDelegate, ConnectionDelegate {
         connection.nick = "Wired-Bot"
         connection.status = "I´m connected (:"
 
-        // perform  connect
+        // perform connect
         if connection.connect(withUrl: url) {
             print("connected")
-            connection.joinChat(chatID: 1)
+            _ = connection.joinChat(chatID: 1)
+            
+            // we keep a reference to the working connection here
+            self.connection = connection
         } else {
             // not connected
             print(connection.socket.errors)
         }
-
-        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    @IBAction func send_text(_ sender: Any) {
+        // to send a test message on the connection we opened at launch
+        
+        // check if the local connection reference is not nil
+        if let connection = self.connection {
+            // if still connected
+            if connection.isConnected() {
+                let message = P7Message(withName: "wired.chat.send_say", spec: connection.spec)
+                // public chat ID : DO NOT FORGET to cast to UInt32 here
+                // this is an inconsistency in the framework that need to be fixed
+                message.addParameter(field: "wired.chat.id", value: UInt32(1))
+                message.addParameter(field: "wired.chat.say", value: "Hello, world!")
+                
+                _ = connection.send(message: message)
+            }
+        }
+   }
 }
-
